b2c036fe3102b8d122b7de5b118f34567e87dae7
Malia Mumo Kisau <125894333+MaliaKisau@users.noreply.github.com>
2024-03-05 23:45:11 +0300

    Update Ceaser Cipher Project.py
    Changes were made for user experience improvements and some final touches to make the code work properly.

--- Ceaser Cipher Project.py
+++ Ceaser Cipher Project.py
@@ -4,15 +4,37 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decyrpt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def ceaser(start_text, shift_amount, cipher_direction):
+def caesar(start_text, shift_amount, cipher_direction):
     end_text = ''
     if cipher_direction == "decode":
         shift_amount = -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+#If the user enters a number/ symbol or space.
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text =+ char
     print(f"The {cipher_direction}d text is {end_text}")
 
-ceaser(start_text=text, shift_amount=shift, cipher_direction=direction)   
+#To ask the user if they want to restart the cipher program.
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to dcrypt:\n")
+    text = input("Type your message:\n")
+    shift = int(input("Type the shift number:\n"))
+    
+#If user enters a shift that is greater than the number of letters in the alphabet.
+    shift = shift % 26
+    
+    ceaser(start_text=text, shift_amount=shift, cipher_direction=direction) 
+    
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
+    
+
+  
     
